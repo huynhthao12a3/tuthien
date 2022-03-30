46afb5f3e63009ba6d5df94e5df0493e93551dbf
Thuantran53102 <tranthuan12a3@gmail.com>
2022-03-30 18:32:50 +0700

    pussh

--- src/views/admin/Project/index.jsx
+++ src/views/admin/Project/index.jsx
@@ -3,14 +3,15 @@
 import Style from "./Project.module.scss"
 import clsx from "clsx"
 import { margin, style } from "@mui/system"
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 import DateRangePicker from 'rsuite/DateRangePicker';
 import { startOfDay, endOfDay, addDays, subDays } from 'date-fns';
 import 'rsuite/dist/rsuite-rtl.min.css'
 import Select from 'react-select'
 
 function Project(){
-    const options = [
+    // select danh muc
+    const filtercategory = [
         { value: 'chocolate', label: 'Chocolate' },
         { value: 'strawberry', label: 'Strawberry' },
         { value: 'vanilla', label: 'Vanilla' }, { value: 'chocolate', label: 'Chocolate' },
@@ -23,6 +24,18 @@ function Project(){
         { value: 'strawberry', label: 'Strawberry' },
         { value: 'vanilla', label: 'Vanilla' }
       ]
+      // select trạng thái
+      const filterStatus = [
+        { value: 'chocolate', label: 'Chocolate' },
+        { value: 'strawberry', label: 'Strawberry' },
+      
+      ]
+      const filterTopic = [
+        { value: 'chocolate', label: 'Chocolate' },
+        { value: 'strawberry', label: 'Strawberry' },
+      
+      ]
+      
       const {
         allowedMaxDays,
         allowedDays,
@@ -59,6 +72,34 @@ function Project(){
         },
     ];
 
+    // biến tạm
+    const status={ 1:'đang chờ duyệt',2:'đang thực thi',3:'hoàng thành'}
+    const category={
+        1:'thiên tai',
+        2:'trẻ em',
+        3:'sức khỏe',
+        4:'con người',
+        5:'xã hội',
+        6:'sức khỏe',
+    }
+    const [arrayPost, setArrayPost] = useState([{
+        "id": 1,
+        "Name": "",
+        "address": "",
+        "category": 1,
+        "createUse": "",
+        "status": 1,
+    }])
+    const [project,setProject]=useState({
+        id:1,
+        Name:'',
+        address:'',
+        category:'',
+        createUse:'',
+        status:1
+    })
+
+
     return(
         <>
             <div className={clsx(Style.project,"main-manage container-fluid")}>
@@ -79,19 +120,19 @@ function Project(){
                             <div className={'mt-4'}>
                                 <h5 className={clsx(Style.searchContent,'')}>Danh mục</h5>
                                 <div className="form-group">
-                                    <Select className={clsx( Style.Inputfocus)} placeholder='danh mục' options={options} />
+                                    <Select className={clsx( Style.Inputfocus)} placeholder='danh mục' options={filtercategory} />
                                 </div>
                             </div>
                             <div className={'mt-4'}>
                                 <h5 className={clsx(Style.searchContent,'')}>Trạng thái</h5>
                                 <div className="form-group">
-                                    <Select className={clsx( Style.Inputfocus)} placeholder='danh mục' options={options} />
+                                    <Select className={clsx( Style.Inputfocus)} placeholder='danh mục' options={filterStatus} />
                                 </div>
                             </div>
                             <div className={'mt-4'}>
                                 <h5 className={clsx(Style.searchContent,'')}>Chủ thể</h5>
                                 <div className="form-group">
-                                    <Select className={clsx( Style.Inputfocus)} placeholder='danh mục' options={options} />
+                                    <Select className={clsx( Style.Inputfocus)} placeholder='danh mục' options={filterTopic} />
                                 </div>
                             </div>
                              <div className="mt-4">
@@ -115,7 +156,47 @@ function Project(){
                         </div>
                     </div>
                     <div className={clsx('list col-9')}>
-
+                        <div className={clsx(Style.listPoject)}>
+                            <div className="page-aside-right">
+                                        <div className={clsx(Style.table_responsive, 'table-responsive')}>
+                                            <table className="table table-hover table-centered">
+                                                <thead>
+                                                    <tr className={clsx(Style.textCenter)} style={{ position: 'relative' }}>
+                                                        <th className="text-center px-w-50">#</th>
+                                                        <th>Mã dự án</th>
+                                                        <th>Tên dự án</th>
+                                                        <th>Địa chỉ</th>
+                                                        <th>Danh mục</th>
+                                                        <th>Người tạo</th>
+                                                        <th>Trạng thái</th>
+                                                        <th className="text-center px-w-50"></th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody id="tbl-body" >
+                                                    <tr className={clsx(Style.textMuted, 'text-muted')} >
+                                                        {/* <td colspan="7" className={clsx(Style.textCenter, "text-center")}>
+                                                            <div className="text-center text-muted pt-4 pb-4">
+                                                                <p className="mb-0">
+                                                                    <i className="mdi mdi-48px mdi-folder-open-outline"> </i>
+                                                                </p>
+                                                                <p>Không tìm thấy dữ liệu</p>
+                                                            </div>
+                                                        </td> */}
+                                                         {/* "id": 1,
+                                                            "Name": "",
+                                                            "address": "",
+                                                            "category": 1,
+                                                            "createUse": "",
+                                                            "status": 1, */}
+                                                        
+                                                     
+                                                    </tr>
+                                                    
+                                                </tbody >
+                                            </table >
+                                        </div >
+                            </div>  
+                        </div>
                     </div>
                 </div>
             </div>
