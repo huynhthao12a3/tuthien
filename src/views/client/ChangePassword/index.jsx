29fff3430ba7473bc5b6317eeabffc59f3051205
Huynh Thao <huynhthao12a3@gmail.com>
2022-05-31 11:27:06 +0700

    update sm, edit change password

--- src/views/client/ChangePassword/index.jsx
+++ src/views/client/ChangePassword/index.jsx
@@ -12,63 +12,77 @@ function ChangePassword(props) {
     const { search } = useLocation();
     const history = useHistory();
     const searchParams = new URLSearchParams(search);
-
-    const [isLoading, setIsLoading] = useState(true);
     const [email, setEmail] = useState();
+
     useEffect(() => {
         setEmail(searchParams.get("email"));
-        // const fetchConfirmEmail = async () => {
-        //     const params = {
-        //         email
-        //     }
-        //     const response = await clientUser.confirmEmail(params)
-        //     if (response.isSuccess) {
-        //         history.push('/dashboard')
-        //         setIsLoading(false)
-        //         swal2.fire({
-        //             title: "Thông báo",
-        //             html: `Kết quả xác minh: ${response.message}`,
-        //             icon: "info",
-        //             confirmButtonColor: 'var(--love-color-1)'
-
-        //         });
-        //     }
-        //     else {
-        //         history.push('/dashboard')
-        //         setIsLoading(false)
-        //         swal2.fire({
-        //             title: "Thông báo",
-        //             html: `${response.message}`,
-        //             icon: "info",
-        //             confirmButtonColor: 'var(--love-color-1)'
-
-        //         });
-
-        //     }
-        // }
-        // fetchConfirmEmail()
     }, [])
     const [newPassword, setNewPassword] = useState()
     const [confirmPassword, setConfirmPassword] = useState()
     const handleChangePassword = async (e) => {
-        e.preventDefault()
         console.log("change password")
         console.log(email)
-        // const response = await clientUser.changePassword(
-        //     {
-        //         email,
-        //         newPassword: ""
-        //     }
-        // )
+        if (newPassword.length > 6) {
+
+            if (newPassword.localeCompare(confirmPassword) === 0) {
+                const response = await clientUser.changePassword(
+                    {
+                        email,
+                        newPassword: newPassword
+                    }
+                )
+                if (response.isSuccess) {
+                    history.push('/dashboard')
+                    swal2.fire({
+                        title: "Thông báo",
+                        html: 'Bạn đã đổi mật khẩu thành công.',
+                        icon: "success",
+                        confirmButtonColor: 'var(--love-color-1)'
+
+                    });
+                }
+                else {
+                    history.push('/dashboard')
+                    swal2.fire({
+                        title: "Thông báo",
+                        html: `${response.message}`,
+                        icon: "info",
+                        confirmButtonColor: 'var(--love-color-1)'
+
+                    });
+
+                }
+            } else {
+                swal2.fire({
+                    title: "Thông báo",
+                    html: `Xác nhận mật khẩu không khớp. Vui lòng nhập lại.`,
+                    icon: "info",
+                    confirmButtonColor: 'var(--love-color-1)'
+
+                });
+
+            }
+        }
+        else {
+            swal2.fire({
+                title: "Thông báo",
+                html: `Mật khẩu phải tối thiểu 6 kí tự. Vui lòng nhập đầy đủ..`,
+                icon: "info",
+                confirmButtonColor: 'var(--love-color-1)'
+
+            });
+
+        }
+
     }
     return (
         <div className="container py-5">
             <div className="row">
-                <form onSubmit={e => handleChangePassword(e)} className="d-flex justify-content-center flex-column">
-                    <input className="form-control my-5 px-3" type="text" placeholder="Mật khẩu mới" onChange={e => setNewPassword(e.target.value)} />
-                    <input className="form-control mb-5 px-3" type="text" placeholder="Nhập lại mật khẩu mới" onChange={e => setConfirmPassword(e.target.value)} />
+                <div className="d-flex justify-content-center flex-column">
+                    <input className="form-control my-5 px-3" type="password" placeholder="Mật khẩu mới" onChange={e => setNewPassword(e.target.value)} />
+                    <input className="form-control mb-5 px-3" type="password" placeholder="Nhập lại mật khẩu mới" onChange={e => setConfirmPassword(e.target.value)} />
                     <button onClick={e => handleChangePassword(e)} className="py-2">Xác nhận</button>
-                </form>
+                </div>
             </div>
         </div>
     );
